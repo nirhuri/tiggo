ccd30728427764e5f6c3dfd0665ef9bf3f5531f7
Nir Huri <hurinir@gmail.com>
2022-07-30 01:18:27 +0300

    adding account and user value objects

---
+++ backend/common/common-domain/src/main/java/com/saver/system/domain/valueobject/UserId.java
@@ -0,0 +1,9 @@
+package com.saver.system.domain.valueobject;
+
+import java.util.UUID;
+
+public class UserId extends BaseId<UUID> {
+    public UserId(UUID value) {
+        super(value);
+    }
+}
