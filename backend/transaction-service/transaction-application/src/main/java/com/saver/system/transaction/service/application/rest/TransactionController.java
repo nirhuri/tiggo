1d21364fcc2cf9773dae6869dffd6e0c6a8b1b07
Nir Huri <hurinir@gmail.com>
2022-08-07 16:27:54 +0300

    adding transaction application controller

---
+++ backend/transaction-service/transaction-application/src/main/java/com/saver/system/transaction/service/application/rest/TransactionController.java
@@ -0,0 +1,14 @@
+package com.saver.system.transaction.service.application.rest;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/transactions", produces = "application/vnd.api.v1+json")
+public class TransactionController {
+
+    //private final TransactionApplicationService transactionApplicationService;
+}
