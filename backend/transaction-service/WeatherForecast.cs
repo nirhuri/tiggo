f35a60fbb5251e3488be35b6e4057c3c8800f935
RON ZAMIR <ronzamiron@gmail.com>
2022-08-17 15:48:55 +0300

    chack

---
+++ backend/transaction-service/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace transaction_service
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 33 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
