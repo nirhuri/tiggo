f6a7a448600bca8d053e42056bb6dba813392b9b
RON ZAMIR <ronzamiron@gmail.com>
2022-08-20 23:44:08 +0300

    formating

--- backend/transaction-service/Controllers/TransactionController.cs
+++ backend/transaction-service/Controllers/TransactionController.cs
@@ -1,17 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 namespace transaction_service.Controllers;
 
-
-
 [ApiController]
-    [Route("transaction")]
-    public class HomeController : ControllerBase
+[Route("transaction")]
+public class HomeController : ControllerBase
 {
-        [HttpGet(Name = "cash")]
-        public string getCashTransaction()
-        {
-            return "Cash Transaction response here...";
-        }
+    [HttpGet(Name = "cash")]
+    public string getCashTransaction()
+    {
+        return "Cash Transaction response here...";
     }
+}
 
 
