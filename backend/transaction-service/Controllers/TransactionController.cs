6a083f90977e257311efa2bccbe7291eb8beb737
RON ZAMIR <ronzamiron@gmail.com>
2022-08-20 23:43:28 +0300

    running transaction

--- backend/transaction-service/Controllers/TransactionController.cs
+++ backend/transaction-service/Controllers/TransactionController.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+namespace transaction_service.Controllers;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace transaction_service.Controllers
+
+[ApiController]
+    [Route("transaction")]
+    public class HomeController : ControllerBase
 {
-    [ApiController]
-    [Route("[transaction]")]
-    public class HomeController : Controller
-    {
         [HttpGet(Name = "cash")]
         public string getCashTransaction()
         {
             return "Cash Transaction response here...";
         }
     }
-}
+
 
