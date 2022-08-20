b983f1cd46075df2610915a2fce0543ece646c1b
Nir Huri <hurinir@gmail.com>
2022-08-20 23:21:56 +0300

    init commit

---
+++ backend/transaction-service/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace transaction_service.Controllers
+{
+    [ApiController]
+    [Route("[transaction]")]
+    public class HomeController : Controller
+    {
+        [HttpGet(Name = "cash")]
+        public string getCashTransaction()
+        {
+            return "Cash Transaction response here...";
+        }
+    }
+}
+
