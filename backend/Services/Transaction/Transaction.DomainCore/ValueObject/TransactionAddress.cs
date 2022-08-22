edd8ccec4548150d22ab8adaeb70229649288332
RON ZAMIR <ronzamiron@gmail.com>
2022-08-23 00:00:06 +0300

    init new project

--- backend/Services/Transaction/Transaction.DomainCore/ValueObject/TransactionAddress.cs
+++ backend/Services/Transaction/Transaction.DomainCore/ValueObject/TransactionAddress.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿
 namespace transaction_domain_core.ValueObject
 {
     public class TransactionAddress
     {
-        public TransactionAddress()
-        {
-        }
+       
     }
 }
 
