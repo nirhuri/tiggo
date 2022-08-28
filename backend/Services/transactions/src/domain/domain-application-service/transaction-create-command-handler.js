f8caa215abe79c3282a8a034b1afdb06eebe963b
Nir Huri <hurinir@gmail.com>
2022-08-29 00:10:17 +0300

    removing packages

--- backend/Services/transactions/src/domain/domain-application-service/transaction-create-command-handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransactionCreateCommandHandler = void 0;
-var TransactionCreateCommandHandler = /** @class */ (function () {
-    function TransactionCreateCommandHandler() {
-    }
-    return TransactionCreateCommandHandler;
-}());
-exports.TransactionCreateCommandHandler = TransactionCreateCommandHandler;
--- backend/Services/transactions/src/domain/domain-core/entity/transaction.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Transaction = void 0;
-var Transaction = /** @class */ (function () {
-    function Transaction() {
-    }
-    return Transaction;
-}());
-exports.Transaction = Transaction;
