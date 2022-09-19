2d18efe891d985f0da95de1cd3ebfe17b889f5ea
Nir Huri <hurinir@gmail.com>
2022-09-19 22:49:45 +0300

    indexing cash_transaction table

--- backend/Services/transaction-service/data-access/repositories/transaction-model.ts
+++ backend/Services/transaction-service/data-access/repositories/transaction-model.ts
@@ -2,34 +2,44 @@ import { DataTypes } from 'sequelize';
 import getDbConnection from '../db-connection';
 
 export default function getTransactionModel() {
-  return getDbConnection().define('cash_transaction', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+  return getDbConnection().define(
+    'cash_transaction',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+      },
+      businessName: {
+        type: DataTypes.STRING,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        unique: true,
+      },
+      amount: {
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        type: DataTypes.STRING,
+      },
+      type: {
+        type: DataTypes.ENUM('WITHDRAW', 'DEPOSITE'),
+      },
     },
-    createdAt: {
-      type: DataTypes.DATE,
-    },
-    updatedAt: {
-      type: DataTypes.DATE
-    },
-    businessName:  {
-      type: DataTypes.STRING,
-      
-    },
-    userId: {
-      type: DataTypes.UUID,
-      unique: true,
-    },
-    amount: {
-      type: DataTypes.INTEGER,
-    },
-    title: {
-      type: DataTypes.STRING,
-    },
-    type: {
-      type: DataTypes.ENUM('WITHDRAW', 'DEPOSITE'),
-    },
-  });
+    {
+      indexes: [
+        {
+          unique: false,
+          fields: ['userId', 'type', 'createdAt'],
+        },
+      ],
+    }
+  );
 }
