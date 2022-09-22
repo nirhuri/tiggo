0b9816e1fb099ccf682e9f328d86816091f68a99
Nir Huri <hurinir@gmail.com>
2022-09-22 23:37:30 +0300

    creating users microservice

---
+++ backend/Services/users/data-access/config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  username: 'postgres',
+  password: '',
+  database: 'transactions',
+  host: 'localhost',
+  port: 5432,
+  logging: false,
+  dialect: 'postgres',
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
---
+++ backend/Services/users/data-access/repositories/transaction-repository.ts
@@ -0,0 +1,20 @@
+import getTransactionModel from './cash-transaction-model';
+
+// ️️️✅ Best Practice: The repository pattern - Wrap the entire DB layer with a simple interface that returns plain JS objects
+export async function getOrderById(id: number) {
+  return await getTransactionModel().findOne({ where: { id } });
+}
+
+export async function addTransaction(transactionDetails) {
+  const addingResponse = await getTransactionModel().create(transactionDetails);
+
+  return addingResponse;
+}
+
+export async function deleteTransaction(orderIdToDelete: number) {
+  await getTransactionModel().destroy({ where: { id: orderIdToDelete } });
+}
+
+export async function cleanupData() {
+  await getTransactionModel().truncate();
+}
