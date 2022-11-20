302893770d0f379ba4861552ef063687b020f139
Nir Huri <hurinir@gmail.com>
2022-11-20 15:17:36 +0200

    adding signin mobile gateway route

--- backend/Gateways/mobile-gateway/entry-points/api/user-routes.ts
+++ backend/Gateways/mobile-gateway/entry-points/api/user-routes.ts
@@ -24,8 +24,26 @@ export function defineRoutes(expressApp: express.Application) {
             true
           );
         });
+      return res.status(HTTP_CODES.CREATED).send(addUserResponse.data);
+    } catch (error: unknown) {
+      return next(error);
+    }
+  });
 
-      return res.status(201).send(addUserResponse.data);
+  router.post('/signin', async (req, res, next) => {
+    try {
+      logger.info(`Gateway API was called to signin a user`, req.body);
+      const signinUserResponse = await axios
+        .post('http://0.0.0.0:3003/users/signin', req.body)
+        .catch((error) => {
+          throw new AppError(
+            'signin-user-error',
+            error.response.data,
+            error.response.status,
+            true
+          );
+        });
+      return res.status(HTTP_CODES.OK).send(signinUserResponse.data);
     } catch (error: unknown) {
       return next(error);
     }
